f761fcf0431b314a53e9af90be455480c1b8db98
Haggai Yaniv <haggai@getjaco.com>
2017-01-21 19:29:06 +0200

    adding nodejs

---
+++ azk/azkdemo-nodejs.js
@@ -0,0 +1,28 @@
+
+module.exports = function (sync, persistent, path) {
+  return {
+    // Dependent systems
+    depends: [],
+    // More images:  http://images.azk.io
+    image: {'docker': 'node:6.9.1'},
+    // Steps to execute before running instances
+    provision: [
+      "npm install",
+    ],
+    workdir: "/azk/code",
+    shell: "/bin/bash",
+    command: "npm start",
+    wait: 10000,
+    mounts: {
+      '/azk/code': sync("./nodejs"),
+      '/azk/code/node_modules': persistent("#{system.name}-node_modules"),
+    },
+    http: {
+      domains: [ "#{system.name}.#{azk.default_domain}" ]
+    },
+    envs: {
+      // set instances variables
+      NODE_ENV: "dev",
+    }
+  };
+}
